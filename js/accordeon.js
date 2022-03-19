07d0fbd34dcc725f4e3b899b8c578affa686562e
Eduard <akimov-eduard@yandex.ru>
2022-03-19 15:02:39 +0300

    hotfix: исправить поведение активной кнопки аккордеона при повторном нажатии на нее

--- js/accordeon.js
+++ js/accordeon.js
@@ -3,11 +3,16 @@ const accordeonButtons = accordeon.querySelectorAll('.feature__link');
 
 accordeonButtons.forEach((btn) => {
   btn.addEventListener('click', () => {
-    accordeonButtons.forEach((button) => {
-      button.classList.remove('feature__link_active');
-      button.nextElementSibling.classList.add('hidden');
-    });
-    btn.classList.toggle('feature__link_active');
-    btn.nextElementSibling.classList.toggle('hidden');
+    if (btn.classList.contains('feature__link_active')) {
+      btn.classList.remove('feature__link_active');
+      btn.nextElementSibling.classList.add('hidden');
+    } else {
+      accordeonButtons.forEach((button) => {
+        button.classList.remove('feature__link_active');
+        button.nextElementSibling.classList.add('hidden');
+      });
+      btn.classList.toggle('feature__link_active');
+      btn.nextElementSibling.classList.toggle('hidden');
+    }
   });
 })
